3261063ab738c3363c8cf09053b8ac7c30eaf192
Sara Raley <sara.m.raley@gmail.com>
2022-12-26 19:33:42 -0700

    adds signup/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  ### PRODUCTS
   get "/products" => "products#index"
 
   post "/products" => "products#create"
@@ -8,4 +10,7 @@ Rails.application.routes.draw do
   patch "/products/:id" => "products#update"
 
   delete "/products/:id" => "products#destroy"
+
+  ### USERS
+  post "/users" => "users#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_010637) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_022830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_010637) do
     t.decimal "price"
     t.string "description"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
