5df8927d77b55dc21d2a53633245d3294eb1d126
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 09:00:35 -0700

    seeds categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   # has_many :orders, through: :carted_products
-  # has_many :carted_products
-  # has_many :category_products
-  # has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   # def is_discounted?
   #   if price < 10
