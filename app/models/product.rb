c206ec1b1dcaea37f1ae62be1beefa5de9454ba1
Sara Raley <sara.m.raley@gmail.com>
2022-12-29 18:24:44 -0700

    updates index, show, controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,24 +4,24 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
 
-  # def is_discounted?
-  #   if price < 10
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
 
-  # def subtotal
-  #   price * quantity
-  # end
+  def subtotal
+    price * quantity
+  end
 
-  # def tax
-  #   tax = price * 0.09
-  # end
+  def tax
+    tax = price * 0.09
+  end
 
-  # def total
-  #   sum = tax + price
-  #   sum
-  # end
+  def total
+    sum = tax + price
+    sum
+  end
 end
